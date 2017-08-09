51f996479b73305581cac486ca2b8e8c995a4627
Andy Fone <fone.andrew@gmail.com>
2017-08-09 17:22:12 +1200

    Swap word API out with Chuck Norris joke API

--- index.js
+++ index.js
@@ -16,17 +16,20 @@ $(function() {
    */
 });
 
-// Gets a random word from an API, then passes it to the callback function
-function fetchRandomWord(callback) {
-  // this api picks a random word and returns it as plain text
-  var promise = $.get("http://setgetgo.com/randomword/get.php");
+// Gets a random chuck norris joke from an API, then passes it to the callback function
+function fetchRandomChuckNorrisJoke(callback) {
+  // this api picks a random chuck norris joke and returns it as plain text
+  // limited to the dev category
+  // WARNING: I do not control the content of the jokes! Some might be a bit rude!
+  var promise = $.get("https://api.chucknorris.io/jokes/random?category=dev");
 
   // $.get is asynchronous, so we need to define a
   // handler for when the request is complete
   promise.done(function(data) {
+    // The data returned is a json object
     console.log(data);
 
     // if you want to do something with the word do it here
-    callback(data);
+    callback(data.value);
   })
 }
