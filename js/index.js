f3b7e7257ef2ca180df3de0e15450fc90173ddbf
Rachel Anderson <andersrach8@ecs.vuw.ac.nz>
2018-08-25 12:25:13 +1200

    created backend js file

--- js/index.js
+++ js/index.js
@@ -50,31 +50,3 @@ function fetchRandomTriviaQuestion(callback) {
     callback(question, answer);
   })
 }
-
-$(document).ready(function() {
-  $.ajax({
-      type: "GET",
-      url: "livestock-data.csv",
-      dataType: "text",
-      success: function(data) {processData(data);}
-   });
-});
-
-function processData(allText) {
-  var allTextLines = allText.split(/\r\n|\n/);
-  var headers = allTextLines[0].split(',');
-  var lines = [];
-
-  for (var i=1; i<allTextLines.length; i++) {
-      var data = allTextLines[i].split(',');
-      if (data.length == headers.length) {
-
-          var tarr = [];
-          for (var j=0; j<headers.length; j++) {
-              tarr.push(headers[j]+":"+data[j]);
-          }
-          lines.push(tarr);
-      }
-  }
-  // alert(lines);
-}
