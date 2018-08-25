6a52bab82fed885538f7d7b910f1c5ed9a8496a8
Rachel Anderson <andersrach8@ecs.vuw.ac.nz>
2018-08-25 14:02:20 +1200

    server now returns sheep population value

--- js/server.js
+++ js/server.js
@@ -8,15 +8,19 @@ app.listen(3000, () => console.log('Example app listening on port 3000!'))
 function processData() {
     var csv = require("csv-query");
     var numbers;
-    var promise = new Promise((reject, resolve) => csv.createFromFile("../livestock-data.csv").then(function (db) {
-      numbers = db.find( { Livestock: 'Total sheep' } );
-      numbers = numbers.value();
-      population = numbers[numbers.length-1];
-      resolve(population.Value);
-    }).then(function (record) {
-      // Do some stuff
-    }).catch(function (error) {
+    var promise = new Promise((resolve, reject) => { csv.createFromFile(
+        "livestock-data.csv"
+      ).then(function (db) {
+        numbers = db.find( { Livestock: 'Total sheep' } );
+        numbers = numbers.value();
+        population = numbers[numbers.length-1];
+        resolve(population.Value);
+      }).then(function (record) {
+        // Do some stuff
+      }).catch(function (error) {
+        throw error;
+    })}).catch(function (error) {
       throw error;
-    }));
+    });
     return promise;
   }
